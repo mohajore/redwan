6fa664ef180ed7c986bddb23937667067f7514f1
Moath Alzaol <moath.alzaol@merwas.net>
2021-09-02 15:53:25 +0300

    create big sale (9/2/2021)

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import AboutBlock from "./AboutBlock";
 import LandingPage from "./LandingPage";
+import BigSale from "./BigSale";
 
 class Home extends Component {
     render() {
@@ -8,6 +9,7 @@ class Home extends Component {
             <div className="home">
                 <LandingPage />
                 <AboutBlock />
+                <BigSale />
             </div>
         );
     }
