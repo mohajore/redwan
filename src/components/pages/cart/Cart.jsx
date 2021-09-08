447a3e46e8652d81c92836c4055665edf7c078e5
mohammad aldokh <mohammad.aldokh@merwas.net>
2021-09-08 15:54:24 +0300

    third edit (9/8/2021)

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Modal } from "react-bootstrap";
 import { displayAlert } from "../../../utils/misc";
 import Product from "../../blocks/Product";
 import TextInput from "../../blocks/TextInput";
@@ -9,9 +9,31 @@ import Location from "../Location/Location";
 import HorizontalProduct from "./HorizontalProduct";
 
 class Cart extends Component {
-  state = { step: 1, qty: 1, selectedLocation: 0, selectedPayment: 0 };
+  state = {
+    step: 1,
+    qty: 1,
+    selectedLocation: 0,
+    selectedPayment: 0,
+
+    AddAddress: false,
+    fields: {
+      Address: "",
+      Name: "",
+    },
+  };
+
+  closeModal = () => {
+    this.setState({
+      AddAddress: false,
+    });
+  };
+
   render() {
-    const { step, qty, selectedLocation, selectedPayment } = this.state;
+    const { step, qty, selectedLocation, selectedPayment, fields, AddAddress } =
+      this.state;
+
+    const onFieldChange = (name, value) =>
+      this.setState({ fields: { ...fields, [name]: value } });
     return (
       <div className="contact-us cart">
         <div className="page-label" />
@@ -121,6 +143,13 @@ class Cart extends Component {
                       changeLocation={(id) => this.selectedLocation(id)}
                       widthCheck
                     />
+
+                    <button
+                      className="submit-button"
+                      onClick={() => this.setState({ AddAddress: true })}
+                    >
+                      Add Location
+                    </button>
                   </>
                 ) : (
                   <>
@@ -207,6 +236,64 @@ class Cart extends Component {
         <NewTitles title="Books you may like" />
 
         <AgentOf />
+
+        {/* add/edit address modal */}
+        <Modal
+          show={AddAddress}
+          width="600"
+          effect="fadeInUp"
+          onHide={this.closeModal}
+          className="location-modal"
+        >
+          <div className="modal-title flex">
+            <h5>Edit Your Location</h5>
+            <i onClick={this.closeModal} className="fa fa-times"></i>
+          </div>
+          <div className="address-inputs">
+            <Row>
+              <Col lg={6} md={12} sm={12} style={{ marginBottom: "1rem" }}>
+                <TextInput
+                  name="Name"
+                  label="Name"
+                  placeholder="Name"
+                  value={fields.Name}
+                  onFieldChange={onFieldChange}
+                />
+              </Col>
+              <Col lg={6} md={12} sm={12} style={{ marginBottom: "1rem" }}>
+                <TextInput
+                  name="Phone"
+                  label="Phone"
+                  placeholder="Phone"
+                  value={fields.Phone}
+                  onFieldChange={onFieldChange}
+                />
+              </Col>
+              <Col lg={12} md={12} sm={12} style={{ marginBottom: "1rem" }}>
+                <TextInput
+                  name="Address"
+                  label="Address"
+                  placeholder="Address"
+                  value={fields.Address}
+                  onFieldChange={onFieldChange}
+                  isTextArea
+                />
+              </Col>
+
+              <Col lg={12} md={12} sm={12} className="locationButton">
+                <button
+                  className="submit-button"
+                  onClick={() => {
+                    //   this.addUpdateUserLocation();
+                  }}
+                >
+                  Add Location
+                </button>
+              </Col>
+            </Row>
+          </div>
+        </Modal>
+        {/* end add/edit address modal */}
       </div>
     );
   }
